d3d5a1c1b2d1f4075529e1de09a5967e39a12cc4
filipclaesson <filipclaesson@hotmail.com>
2017-04-08 17:02:30 +0200

    remove code that gets sl dataä

--- main.js
+++ main.js
@@ -39,32 +39,29 @@ var SendBooliDataToSLAPI = function(booliApartments){
 		console.log("--------------------------------------------------")
 		console.log("Step 3: store Booli data in DB ... count: " + Apartments.length);
 		console.log("--------------------------------------------------")
-		Postgres.insertMultiApartments(Apartments,db, checkGeoPositionAndSendToSL())
-		
-		
-		
+		Postgres.insertMultiApartments(Apartments,db, function(){console.log(" ------ Everything is finished ------")})
 	}
 }
-function getNext(){
-	counter = counter + 1;
-	if (counter < geoLocationsToLookup.length){
-		var func = SL.calcDistVars(geoLocationsToLookup[counter].lon, geoLocationsToLookup[counter].lat, 'apts', saveSLData)
-	}
-	else{
-		console.log("--------------------------------------------------")
-		console.log("Step 5: SL Data Complete ...");
-		console.log("--------------------------------------------------")
-		console.log("--------------------------------------------------")
-		console.log(" Step 6: Send SL  DB ...");
-		console.log("--------------------------------------------------")
+// function getNext(){
+// 	counter = counter + 1;
+// 	if (counter < geoLocationsToLookup.length){
+// 		var func = SL.calcDistVars(geoLocationsToLookup[counter].lon, geoLocationsToLookup[counter].lat, 'apts', saveSLData)
+// 	}
+// 	else{
+// 		console.log("--------------------------------------------------")
+// 		console.log("Step 5: SL Data Complete ...");
+// 		console.log("--------------------------------------------------")
+// 		console.log("--------------------------------------------------")
+// 		console.log(" Step 6: Send SL  DB ...");
+// 		console.log("--------------------------------------------------")
 		
-		Postgres.insertMultiSLgeo(geoLocationsToLookup,db, function(){console.log(" ------ Everything is finished ------")})
-	}	
-}
+// 		Postgres.insertMultiSLgeo(geoLocationsToLookup,db, function(){console.log(" ------ Everything is finished ------")})
+// 	}	
+// }
 
-function getNextDelay(){
-	setTimeout(getNext, 3000);
-}
+// function getNextDelay(){
+// 	setTimeout(getNext, 3000);
+// }
 
 /*  ------  MAIN  ------
  * Step 1: Send data to Booli ...
@@ -84,32 +81,32 @@ console.log(startDate + " " + endDate);
 booli.getApartments(process.argv[2], process.argv[3], SendBooliDataToSLAPI);
 
 
-function isLookupPosition(geopos, positionsToday){
-	isLookup = true;
-	for (j in positionsToday){
-		if (positionsToday[j]["lon"] == geopos["lon"] && positionsToday[j]["lat"] == geopos["lat"]){
-			isLookup = false;
-		}
-	}
-	return isLookup;
-}
+// function isLookupPosition(geopos, positionsToday){
+// 	isLookup = true;
+// 	for (j in positionsToday){
+// 		if (positionsToday[j]["lon"] == geopos["lon"] && positionsToday[j]["lat"] == geopos["lat"]){
+// 			isLookup = false;
+// 		}
+// 	}
+// 	return isLookup;
+// }
 
-function checkGeoPositionAndSendToSL(callback){
-	console.log("--------------------------------------------------")
-	console.log("Step 4: sending necessary booli data to SL ...");
-	console.log("--------------------------------------------------")
-	Postgres.runQuery("select lon,lat from geo_data_sl", db, function(data){
-			for (i in Apartments){
-					appGeoPos = {lon: parseFloat(Apartments[i].lon).toFixed(3), lat: parseFloat(Apartments[i].lat).toFixed(3)};
-					if (isLookupPosition(appGeoPos, data.data)){
-						geoLocationsToLookup.push(appGeoPos);
-					}
-			}
-			console.log("Geolocation to send to SL count: " + geoLocationsToLookup.lenght)
-			console.log(geoLocationsToLookup)
-			if (geoLocationsToLookup.length > 0){
-				SL.calcDistVars(geoLocationsToLookup[counter].lon, geoLocationsToLookup[counter].lat, 'apts', saveSLData);
-			}
-	})
+// function checkGeoPositionAndSendToSL(callback){
+// 	console.log("--------------------------------------------------")
+// 	console.log("Step 4: sending necessary booli data to SL ...");
+// 	console.log("--------------------------------------------------")
+// 	Postgres.runQuery("select lon,lat from geo_data_sl", db, function(data){
+// 			for (i in Apartments){
+// 					appGeoPos = {lon: parseFloat(Apartments[i].lon).toFixed(3), lat: parseFloat(Apartments[i].lat).toFixed(3)};
+// 					if (isLookupPosition(appGeoPos, data.data)){
+// 						geoLocationsToLookup.push(appGeoPos);
+// 					}
+// 			}
+// 			console.log("Geolocation to send to SL count: " + geoLocationsToLookup.lenght)
+// 			console.log(geoLocationsToLookup)
+// 			if (geoLocationsToLookup.length > 0){
+// 				SL.calcDistVars(geoLocationsToLookup[counter].lon, geoLocationsToLookup[counter].lat, 'apts', saveSLData);
+// 			}
+// 	})
 
-}
+// }
